6a2b09ffecc12b11c9aa3339a426e3464723153b
Thunder <30287553+yanlei2017@users.noreply.github.com>
2021-03-29 22:07:33 +0800

    Update 1.c
    123

--- 1.c
+++ 1.c
@@ -7,4 +7,42 @@
  * @FilePath: \exam\1.c
  */
 
-2
+
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct node{
+	string name;
+	int id;
+	int val;
+	bool friend operator<(node x,node y){
+		if(x.name!=y.name){
+			return x.name<y.name;
+		}
+		else {
+			if(x.val!=y.val){
+				return x.val<y.val;
+			}
+			else{
+				return x.id<y.id;
+			}
+		}
+	}
+}p[100005];
+
+int main(){
+
+	int n;
+	cin>>n;
+	int cnt=0;
+	while(cin>>p[cnt].name>>p[cnt].id>>p[cnt].val){
+		cnt++;
+	}
+	sort(p,p+cnt);
+	for(int i=0;i<cnt;i++){
+		cout<<p[i].name<<" "<<p[i].id<<" "<<p[i].val<<endl; 
+	}
+	return 0; 
+}
